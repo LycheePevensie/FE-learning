7998f7cf8d2744fd492ebf8f0c56fecc42ca5ae3
Lychee <cnsxgyz@163.com>
2016-10-24 22:00:06 +0800

    10.24

---
+++ algorithm/selectionSort.js
@@ -0,0 +1,36 @@
+/**
+ * Created by Lychee on 2016/10/24.
+ */
+var arr=[3,44,38,5,47,15,36,26,27,2,46,4,19,50,48];
+
+//找最小值
+function selectionSort(arr){
+    var temp,minIndex;
+    for(var i=0;i<arr.length;i++){
+        minIndex=i;
+        for(var j=i+1;j<arr.length;j++){
+            if(arr[j]<arr[minIndex]){
+                minIndex=j;
+            }
+        }
+        temp=arr[i];arr[i]=arr[minIndex];arr[minIndex]=temp;
+    }
+    return arr;
+}
+console.log(selectionSort(arr));
+
+//找最大值
+function selectionSort2(arr){
+    var temp,maxIndex;
+    for(var i=arr.length;i>0;--i){
+        maxIndex=i;
+        for(var j=0;j<i;j++){
+            if(arr[j]>arr[maxIndex]){
+                maxIndex=j;
+            }
+        }
+        temp=arr[i];arr[i]=arr[maxIndex];arr[maxIndex]=temp;
+    }
+    return arr;
+}
+console.log(selectionSort2(arr));
